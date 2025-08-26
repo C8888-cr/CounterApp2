b14e722b987f108df96df418165d6bc7bdf8f30e
Christiane <Christiane>
2025-08-26 15:54:07 +0200

    Changed imports. actually KMP works

--- composeApp/src/androidMain/kotlin/com/example/counterapp2/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/counterapp2/App.kt
@@ -1,13 +1,16 @@
 package com.example.counterapp2
 
+import CounterViewModel
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -18,7 +21,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import counterapp2.composeapp.generated.resources.Res
 import counterapp2.composeapp.generated.resources.compose_multiplatform
-
+import androidx.lifecycle.viewmodel.compose.viewModel
 @Composable
 fun App(viewModel: CounterViewModel = viewModel()) {
     MaterialTheme {
--- shared/src/commonMain/kotlin/com/example/counterapp2/CounterViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/counterapp2/CounterViewModel.kt
@@ -1,28 +1,46 @@
+import com.example.counterapp2.CounterState
 import com.rickclephas.kmp.observableviewmodel.MutableStateFlow
 import com.rickclephas.kmp.observableviewmodel.ViewModel
-import kotlinx.coroutines.CoroutineScope
+
+import com.rickclephas.kmp.observableviewmodel.launch
+
 import kotlinx.coroutines.flow.update
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
+import com.rickclephas.kmp.observableviewmodel.ViewModelScope
 
-//import kotlinx.coroutines.flow.StateFlow
-//import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 //import kotlinx.coroutines.launch
 //import kotlinx.coroutines.delay
 
+
+
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+
 
 open class CounterViewModel: ViewModel() {
-    // Erzeuge einen CoroutineScope, der an die Lebenszeit des ViewModels gebunden ist
+    // Erzeuge einen CoroutineScope, der an
+
+    //
+    // die Lebenszeit des ViewModels gebunden ist
     //override val viewModelScope = CoroutineScope(Dispatchers.Main)
 
     // Privater, veränderlicher StateFlow für interne Updates
-    private val _uiState = MutableStateFlow(CounterState())
+    private val _uiState = MutableStateFlow<CounterState>(viewModelScope as ViewModelScope, CounterState())
     // Öffentlicher, nur lesbarer StateFlow für die UI
 
     //Add NativeCou....
-    @NativeCoroutineState
+    @NativeCoroutinesState
     val uiState: StateFlow<CounterState> = _uiState.asStateFlow()
 
     fun increment() {
@@ -46,9 +64,11 @@ open class CounterViewModel: ViewModel() {
             _uiState.update { it.copy(count = it.count + 1, isLoading = false) }
         }
     }
+/*
+
 
     // Wichtig, um Speicherlecks zu vermeiden, wenn das VM nicht mehr gebraucht wird
     fun clear() {
         viewModelScope.cancel()
-    }
+    }*/
 }
